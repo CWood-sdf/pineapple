2233db924695ca9cab592c5659b74d9cd8d07247
CWood-sdf <woodchristophermichael@gmail.com>
2023-09-02 20:58:56 -0400

    yo

--- lua/themer/init.lua
+++ lua/themer/init.lua
@@ -3,6 +3,9 @@ local M = {}
 local values = nil
 M.setup = function(opts)
     values = require("themer.data")
+    vim.api.nvim_create_user_command("Themer", function(opts)
+        print("Themer")
+    end)
 end
 
 return M
