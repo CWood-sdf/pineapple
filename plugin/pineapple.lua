db47a9d8949aa55a45631f5b3ac9c2eb85125dc1
CWood-sdf <woodchristophermichael@gmail.com>
2023-11-01 04:36:16 -0400

    comment

--- plugin/pineapple.lua
+++ plugin/pineapple.lua
@@ -1,5 +1,6 @@
 vim.api.nvim_create_user_command("Pineapple", function(_)
     local pineapple = require("pineapple")
+    -- do actualSetup bc setup doesn't do anything
     pineapple.actualSetup()
     require('pineapple.ui.buffer').openWindow()
 end, {})
