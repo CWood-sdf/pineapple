a33f483890f1c590aaf122fe6368d806ce36627f
CWood-sdf <woodchristophermichael@gmail.com>
2023-12-25 22:51:51 -0500

    update packages

---
+++ updater/scrape_neovimcraft.sh
@@ -0,0 +1,4 @@
+curl https://nvim.sh/t/tree-sitter-supported-colorscheme?format=json -o tsscolscheme.json
+curl https://nvim.sh/t/colorscheme?format=json -o colscheme.json
+curl https://nvim.sh/t/treesitter-colorschemes?format=json -o tsscheme.json
+curl https://nvim.sh/t/lua-colorscheme?format=json -o luascheme.json
