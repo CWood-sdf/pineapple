6187d6c1b2b61cd54e224b48d9d20c8154db7fd0
CWood-sdf <woodchristophermichael@gmail.com>
2023-12-28 05:00:04 -0500

    readd workers

---
+++ updater/nvim_worker/lua/stuff/lazy.lua
@@ -0,0 +1,2 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
---
+++ updater/nvim_worker/lua/stuff/pineapple.lua
@@ -0,0 +1,9 @@
+return {
+    "catppuccin/nvim",
+    "folke/tokyonight.nvim",
+    "embark-theme/vim",
+    "nordtheme/vim",
+    "daschw/leaf.nvim",
+    "astridlyre/moonlight.nvim",
+    "morhetz/gruvbox",
+}
