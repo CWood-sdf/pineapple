4d4c4d4dac1ccb2cb0d4cdc25c4f0533de09b280
CWood-sdf <woodchristophermichael@gmail.com>
2024-04-24 14:31:00 -0400

    fix: up thread count

--- updater/src/main.rs
+++ updater/src/main.rs
@@ -872,7 +872,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("{}", cli.force);
     let file_lock: Arc<Mutex<bool>> = Arc::new(true.into());
     let repo_locks = Arc::new(Mutex::new(Vec::new()));
-    let thread_count = 10;
+    let thread_count = 64;
 
     // You can check for the existence of subcommands, and if found use their
     // matches just as you would the top level cmd
