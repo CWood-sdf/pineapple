d49bd7b10bae24f6ee718ecbfc75fd1ec6cdd80c
CWood-sdf <woodchristophermichael@gmail.com>
2024-04-24 14:35:57 -0400

    feat: only worker test stuff

--- updater/run.sh
+++ updater/run.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-bash ./scrape_gh.sh
-
-bash ./scrape_neovimcraft.sh
-
-cargo run --release -- clean-import
-
-cargo run --release -- make-color-data
+# bash ./scrape_gh.sh
+#
+# bash ./scrape_neovimcraft.sh
+#
+# cargo run --release -- clean-import
+#
+# cargo run --release -- make-color-data
 
 cargo run --release -- generate-ts
 
